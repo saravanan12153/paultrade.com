883a4208400833367a7e37b24a544c90df81ddf4
Sulman Baig <sulman1990@hotmail.com>
2015-06-18 02:19:41 +0200

    allow users to edit profile without providing current password

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'registrations'}
   resources :posts, only: [:index, :show]
   root 'pages#home'
 
